1dc9523c1ce67d13b927c3a67e6f9a486b1eac0a
kenta-pro <kenta.yuri1026@gmail.com>
2022-06-07 05:07:27 +0900

    first commit

---
+++ src/shaders/fragmentShader.glsl
@@ -0,0 +1,13 @@
+uniform vec3 uColor;
+uniform sampler2D uTexture;
+
+varying vec2 vUv;
+varying float vElevation;
+
+
+void main() {
+  vec4 textureColor = texture2D(uTexture, vUv);
+  // gl_FragColor = vec4(uColor, 1.0);
+  textureColor.rgb *= vElevation * 2.5 + 0.7;
+  gl_FragColor = textureColor;
+}
